ce5d811bb71336dd20ad5fe33a6201d2c617c0ca
itschristianpantilon <pantilonchristian@gmail.com>
2025-04-24 12:11:45 +0800

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { Footer, Home, Navbar } from './components'
+
+import './App.css'
+
+function App() {
+  return (
+    <>
+     <Navbar />
+     <Home />
+     <Footer />
+    </>
+  )
+}
+
+export default App
---
+++ src/components/index.js
@@ -0,0 +1,9 @@
+import Footer from "./Footer/Footer";
+import Home from "./Home/Home";
+import Navbar from "./Navbar/Navbar";
+
+export {
+    Footer,
+    Home,
+    Navbar
+}
